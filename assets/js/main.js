71bd25ce256289e6c0a6c6aad6be5a5935d875e1
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-10-15 15:53:30 +0200

    start js|write down logic&initialize btn variables

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,3 @@
-/*
-Creare un carosello come nella foto allegata. 
-Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e testo.
-Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre.
-
-BONUS:
-Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
-*/
-
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -30,3 +21,24 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+/* INSTRUCTIONS
+Creare un carosello come nella foto allegata. 
+Al click dell'utente sulle frecce verso l'alto o verso il basso, l'immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi titolo e testo.
+Allo stesso tempo nelle miniature l'immagine attiva dovrà apparire in evidenza rispetto alle altre.
+
+BONUS:
+Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso l'alto, la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso il basso.
+*/
+
+//Strumenti: addEventListener() | array[] 
+
+
+//seleziono i pulsanti dalla DOM
+const scrollBtnUp = document.getElementById('btn_up')
+const scrollBtnDown = document.getElementById('btn_down')
+console.log(scrollBtnUp,scrollBtnDown);
+
+//Per ogni click, seleziono un elemento dagli array
+
+
